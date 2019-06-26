207257a087aa74d7d2da675faa39a748f32018bb
metalan <miloRAMBALDIsd6>
2019-06-26 20:49:38 +0100

    hecho sustituir, empezando convertir

--- ej1/clases.py
+++ ej1/clases.py
@@ -4,18 +4,28 @@
 
 class Romano(object):
     original = "0"
+    __romanos = {1:"I", 5:"V", 10:"X", 50:"L", 100:"C", 500:"D", 1000:"M"}
+    __romanos2 = ["0", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]  # Privado para sustituir
     __adaptados = [] # Privado
-    __romanos = ["0", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"] # Privado
+    __convertidos = [] # Privado
 
     def __init__(self): # Al crear
         pass
 
-    def convertir(self, numero):
+    def sustituir(self, numero):
         self.original = str(numero)
-        self.__convertir_original()
+        self.__sustitucion_romana()
         print(str(self.__adaptados))
 
-    def __convertir_original(self): #Privado
+    def convertir(self, numero):
+        self.original = str(numero)
+        self.__conversion_romana()
+        print(str(self.__convertidos))
+
+    def __sustitucion_romana(self): #Privado
         self.__adaptados = list(self.original)
         for x in range(0, len(self.__adaptados)):
-            self.__adaptados[x] = self.__romanos[int(self.__adaptados[x])]
+            self.__adaptados[x] = self.__romanos2[int(self.__adaptados[x])]+
+
+    def __conversion_romana(self): #Privado
+        pass
