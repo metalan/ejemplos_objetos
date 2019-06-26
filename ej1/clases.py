e02850baeac6826777ac14cfd33434e7725a8644
metalan <miloRAMBALDIsd6>
2019-06-26 21:41:31 +0100

    eliminados errores y warnings

--- ej1/clases.py
+++ ej1/clases.py
@@ -4,12 +4,12 @@
 
 class Romano(object):
     original = "0"
-    __romanos = {1:"I", 5:"V", 10:"X", 50:"L", 100:"C", 500:"D", 1000:"M"}
+    __romanos = {1: "I", 5: "V", 10: "X", 50: "L", 100: "C", 500: "D", 1000: "M"}  # Privado para convertir
     __romanos2 = ["0", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]  # Privado para sustituir
-    __adaptados = [] # Privado
-    __convertidos = [] # Privado
+    __adaptados = []  # Privado
+    __convertidos = []  # Privado
 
-    def __init__(self): # Al crear
+    def __init__(self):  # Al crear
         pass
 
     def sustituir(self, numero):
@@ -19,13 +19,16 @@ class Romano(object):
 
     def convertir(self, numero):
         self.original = str(numero)
-        self.__conversion_romana()
-        print(str(self.__convertidos))
+        if int(self.original) <= 1000:
+            self.__conversion_romana()
+            print(str(self.__convertidos))
+        else:
+            print("El límite es mil (1000)")
 
-    def __sustitucion_romana(self): #Privado
+    def __sustitucion_romana(self):  # Privado
         self.__adaptados = list(self.original)
         for x in range(0, len(self.__adaptados)):
-            self.__adaptados[x] = self.__romanos2[int(self.__adaptados[x])]+
+            self.__adaptados[x] = self.__romanos2[int(self.__adaptados[x])]
 
-    def __conversion_romana(self): #Privado
+    def __conversion_romana(self):  # Privado
         pass
