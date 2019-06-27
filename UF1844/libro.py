e202b4b0f1660e85feab318cbdf96b3be791af3b
metalan <miloRAMBALDIsd6>
2019-06-27 23:31:48 +0100

    base de libro

--- UF1844/libro.py
+++ UF1844/libro.py
@@ -1,1 +1,33 @@
 # Author Vladimir SR
+
+class Libro(object):
+    __isbn = ""
+    __titulo = ""
+    __autor = 0
+
+    def __init__(self, titulo, autor, isbn):
+        pass
+
+    def get_isbn(self):
+        pass
+
+    def set_isbn(self):
+        pass
+
+    def get_titulo(self):
+        pass
+
+    def set_titulo(self):
+        pass
+
+    def get_autor(self):
+        pass
+
+    def set_autor(self):
+        pass
+
+    def check_isbn10(self):
+        pass
+
+    def check_isbn13(self):
+        pass
