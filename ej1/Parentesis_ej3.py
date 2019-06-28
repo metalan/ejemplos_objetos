5a040c4ee55930ef6e117cd31909c9f265e84c9d
MetaLan <50374689+metalan@users.noreply.github.com>
2019-06-28 08:46:39 +0100

    update

---
+++ ej1/Parentesis_ej3.py
@@ -0,0 +1,9 @@
+# Author Vladimir SR
+"""
+Escribir una clase en python para encontrar la validez
+de una cadena de paréntesis, '(', ')', '{', '}', '[' '].
+Los paréntesis deben aparecer en el orden correcto, por
+ejemplo "()" y "()[]{}" son válidos, pero "[)", "({[)]"
+y "{{{" son inválidos.
+"""
+
